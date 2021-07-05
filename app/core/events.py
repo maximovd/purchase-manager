86f943a89348d8386e8e54303e839b15de081395
Danil Maximov <bazinga.mail@yandex.ru>
2021-07-05 22:24:54 +0300

    Add models

--- app/core/events.py
+++ app/core/events.py
@@ -7,7 +7,7 @@ def create_start_app_handler(app: FastAPI) -> Callable:  # type: ignore
     register_tortoise(
         app,
         db_url=DATABASE_URL,
-        modules={"models": ["models"]},
+        modules={"models": ["categories", "products", "users"]},
         generate_schemas=True,
         add_exception_handlers=True,
     )
